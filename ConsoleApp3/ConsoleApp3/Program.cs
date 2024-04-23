ab3b82128c18e4783657de976e1e65370258585f
ovamunous <163103581+ovamunous@users.noreply.github.com>
2024-04-23 13:12:20 +0300

    задача 1

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите два целых числа:");
+        Console.Write("Первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int maxNumber = Math.Max(number1, number2);
+        Console.WriteLine($"Максимальное число: {maxNumber}");
+        Console.ReadKey();
+    }
+}
